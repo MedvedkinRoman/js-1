6ca923d260cf14fb0224992733d8bba8f6ed58aa
MedvedkinRoman <78662557+MedvedkinRoman@users.noreply.github.com>
2021-05-13 00:45:12 +0300

    2/3 module 1

--- js/script-1.js
+++ js/script-1.js
@@ -107,3 +107,66 @@ let elementHeight = '220.323px';
 
 // console.log(result);
 // // console.log(Math.round(6.5));
+
+//================ длина стоки, свойство length ======================
+
+// const massege = 'dfjsfdsjdfljslfdk';
+// console.log(massege.length);
+
+//===================== конкатенация строк =======================
+
+const firstName = 'Chelsy';
+const lastName = 'dfddf';
+
+// console.log(firstName + ' ' + lastName);
+
+//================== задачка + шаблонная строка ==========================
+/* напииш скрипт который выведет строку в формате
+* "Гость х y поселяется в z номер q",
+*/
+
+// const room = 732;
+// const type = "VIP";
+
+// // const welcomeMsg = "Гость " + firstName + ' ' + lastName + " поселяется в " + type + " номер " + room;
+// const welcomeMsg = `Гость ${firstName} ${lastName} поселяется в ${type} номер ${room}`;
+// console.log(welcomeMsg);
+
+// const quantity = 15;
+// const orderMsg = `Вы заказываете ${quantity} холодильников`;
+// console.log(orderMsg);
+
+//===================== нормализация с методом =================
+
+let brand = 'SamsuNg';
+// brand = brand.toLowerCase();
+
+// console.log(brand[4]);
+// console.log(brand.slice(1).toLowerCase());
+
+brand = brand[0] + brand.slice(1).toLowerCase();
+
+
+// console.log(brand);
+
+
+
+//==================== поиск в строке includes ===================
+
+const blacklistedWord1 = 'спам';
+const blacklistedWord2 = 'распродажа';
+
+// const string1 = 'Привет, я принц Абдул, это не спам, предлогаю тебе миллион!';
+// const string2 = 'Самая большая РАСПРОДАЖА этой недели, не пропустите!';
+// const string3 = 'рРекламная кампания #fatlivesmatter';
+
+// console.log(string1.includes(blacklistedWord1));
+// console.log(string1.includes(blacklistedWord2));
+
+// console.log(string2.includes(blacklistedWord1));
+
+// const normalizedString2 = string2.toLowerCase();
+// console.log(normalizedString2.includes(blacklistedWord2));
+
+// console.log(string3.includes(blacklistedWord1));
+// console.log(string3.includes(blacklistedWord2));
